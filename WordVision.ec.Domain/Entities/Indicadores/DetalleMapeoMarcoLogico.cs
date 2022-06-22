65d9d582a29e1c0bfd40908c70da098079a83d3c
Satizabal Mosquera Orlyn Daniel <oniel_satizabal@hotmail.com>
2022-06-22 15:42:18 -0500

    DetalleMapeoMarcoLogico

--- WordVision.ec.Domain/Entities/Indicadores/DetalleMapeoMarcoLogico.cs
+++ WordVision.ec.Domain/Entities/Indicadores/DetalleMapeoMarcoLogico.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WordVision.ec.Domain.Contracts;
 
 namespace WordVision.ec.Domain.Entities.Indicadores
 {
-    public class DetalleMapeoMarcoLogico
+    public class DetalleMapeoMarcoLogico : AuditableEntity
     {
-
+        
     }
 }
