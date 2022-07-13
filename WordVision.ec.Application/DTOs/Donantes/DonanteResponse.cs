d8238db4720982a871b7c929c36781be7e4b61e4
Jorge Moreno <jorge_luis_my@hotmail.com>
2022-07-13 11:00:33 -0500

    Cambio de Lista de donantes

---
+++ WordVision.ec.Application/DTOs/Donantes/DonanteResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordVision.ec.Application.DTOs.Donantes
+{
+    public class DonanteResponse
+    {
+        public string Campana { get; set; }
+        public string Donante { get; set; }
+        public string Cedula { get; set; }
+        public string Estado { get; set; }
+        public string Ciudad { get; set; }
+        public decimal Cantidad { get; set; }
+    }
+}
--- WordVision.ec.Infrastructure.Data/Repositories/Donacion/DonanteRepository.cs
+++ WordVision.ec.Infrastructure.Data/Repositories/Donacion/DonanteRepository.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WordVision.ec.Application.DTOs.Debitos;
+using WordVision.ec.Application.DTOs.Donantes;
 using WordVision.ec.Application.Interfaces.Repositories.Donacion;
 using WordVision.ec.Application.Interfaces.Repositories.Registro;
 using WordVision.ec.Application.Interfaces.Repositories.Soporte;
@@ -47,9 +48,45 @@ namespace WordVision.ec.Infrastructure.Data.Repositories.Donacion
             return await _repository.Entities.Where(x => x.Id == idDonante).FirstOrDefaultAsync();
         }
 
-        public async Task<List<Donante>> GetListAsync()
+        public async Task<List<DonanteResponse>> GetListAsync()
         {
-            return await _repository.Entities.ToListAsync();
+            // return await _repository.Entities.ToListAsync();
+            var resultado1 = _repository.Entities
+                                      .Select(a => new DonanteResponse
+                                      {
+                                          Campana    = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 26 && c.Secuencia == a.Campana.ToString()).FirstOrDefault().Nombre,
+                                          //  Id = a.Id,
+                                          //  FechaCaptacion = Convert.ToDateTime(a.FechaConversion),
+                                          //  Canal = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 22 && c.Secuencia == a.Canal.ToString()).FirstOrDefault().Nombre,
+                                          //  Frecuencia = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 34 && c.Secuencia == a.FrecuenciaDonacion.ToString()).FirstOrDefault().Nombre,
+                                          //  Genero = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 28 && c.Secuencia == a.Genero.ToString()).FirstOrDefault().Nombre,
+                                          ////Provincia = ,
+                                          //  NombreDonante = a.Apellido1 + " " + a.Apellido2 + " " + a.Nombre1 + " " + a.Nombre2,
+                                          //  Nombre1 = a.Nombre1,
+                                          //  Nombre2 = a.Nombre2,
+                                          //  Apellido1 = a.Apellido1,
+                                          //  Apellido2 = a.Apellido2,
+                                          //  Identificacion = a.RUC,
+                                          //  FormaPago = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 21 && c.Secuencia == a.FormaPago.ToString()).FirstOrDefault().Nombre,
+                                          ////EntidadBancaria = ,
+                                          //  TipoCuenta = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 35 && c.Secuencia == a.TipoCuenta.ToString()).FirstOrDefault().Nombre,
+                                          //  Cuenta = a.NumeroCuenta,
+                                          //  Valor = a.Cantidad,
+
+                                          ////Mes = ,
+                                          ////Anio = ,
+                                          //  CodigoSCI = Convert.ToInt32(a.Banco),
+                                          //  Categoria = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 25 && c.Secuencia == a.Categoria.ToString()).FirstOrDefault().Nombre,
+                                          //  TipoDonante = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 24 && c.Secuencia == a.Tipo.ToString()).FirstOrDefault().Nombre,
+                                          ////EstadoDebito = ,
+                                          ////FechaDebito = ,
+                                          //  Estado = _repositoryDetalle.Entities.Where(c => c.IdCatalogo == 27 && c.Secuencia == a.EstadoDonante.ToString()).FirstOrDefault().Nombre,
+
+
+                                      }
+                                      ).ToListAsync();
+
+            return await resultado1;
         }
 
         public async Task<int> InsertAsync(Donante donante)
